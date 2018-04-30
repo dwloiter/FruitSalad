686a877f62cda6bb88cb785f3296ca25074fe4b3
Emma Drumheller <elmdrumheller@gmail.com>
2018-04-30 10:37:30 -0700

    Added Game Window
    Responds to button input, uses canvas.

--- javascript/script.js
+++ javascript/script.js
@@ -6,7 +6,7 @@
 var canvas = document.getElementById('gamecanvas');
 var context = canvas.getContext('2d');
 
-var gameStarted = false;
+var gameStart = false;
 
 
 
@@ -19,17 +19,18 @@ var gameStarted = false;
 function main_menu(){
     /* Main Title */
 	context.font = "45px Courier";
-	context.fillStyle = "#928472";
+	context.fillStyle = "#653789";
 	context.textAlign = "center";
 	context.fillText("HTML5 Game", canvas.width/2, canvas.height/2);
+    
     /* Instructions */
 	context.font = "15px Courier";
-	context.fillText("Press ENTER To Start", canvas.width/2, canvas.height/2 + 50);
+	context.fillText("Press ENTER To Start", canvas.width/2, canvas.height/2 + 35);
 }
 
 /*  Start game function. */
 function startGame(){
-	gameStarted = true;
+	gameStart = true;
 	clear();
 
     
@@ -53,8 +54,8 @@ function clear(){
 */
 /* Event listener: If ENTER is pressed... */
 document.body.addEventListener("keydown", function(event){
-    // If ENTER (keyCode 13) is pressed...
-	if(event.keyCode == 13 && !gameStarted){
+    // If ENTER (keyCode 13) is pressed AND game is not started.
+	if(event.keyCode == 13 && !gameStart){
 		startGame(); // Run startGame
 	}
 
